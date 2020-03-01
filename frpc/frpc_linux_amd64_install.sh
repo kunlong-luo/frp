37549b7f7c6c1ec2174e427e4d3bbc565a7151a8
tony·parker <15874751507@163.com>
2020-03-01 12:19:40 +0800

    update shell

--- frpc/frpc_linux_amd64_install.sh
+++ frpc/frpc_linux_amd64_install.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/bash
 
 FRP_VERSION="0.31.2"
-FRP_DIR="/usr/local/frpc"
+FRP_DIR="/usr/bin/"
+FRP_CONF_DIR="/etc/frp/"
 SERVICE_DIR="/lib/systemd/system"
-REPO="tony-parker-luo/frp"
 WD="/tmp"
 
-# create frpc directory
-mkdir -p ${FRP_DIR}
+# create frp config directory
+mkdir -p ${FRP_CONF_DIR}
 
 # download frp package
 wget -P ${WD} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 cd frp_${FRP_VERSION}_linux_amd64 && \
-mv frpc frpc.ini ${FRP_DIR}
-
-# download frpc.service
-wget -P ${SERVICE_DIR} https://raw.githubusercontent.com/${REPO}/master/frpc/frpc.service
+cp frpc ${FRP_DIR} && \
+cp frpc.ini ${FRP_CONF_DIR} && \
+cp ./system/frpc.service ${SERVICE_DIR} && \
 systemctl daemon-reload
 
 # start frpc
--- frps/frps_linux_amd64_install.sh
+++ frps/frps_linux_amd64_install.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/bash
 
 FRP_VERSION="0.31.2"
-FRP_DIR="/usr/local/frps"
+FRP_DIR="/usr/bin/"
+FRP_CONF_DIR="/etc/frp/"
 SERVICE_DIR="/lib/systemd/system"
-REPO="tony-parker-luo/frp"
 WD="/tmp"
 
 # create frps directory
-mkdir -p ${FRP_DIR}
+mkdir -p ${FRP_CONF_DIR}
 
 # download frp package
 wget -P ${WD} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 cd frp_${FRP_VERSION}_linux_amd64 && \
-mv frps frps.ini ${FRP_DIR}
-
-# download frps.service
-wget -P ${SERVICE_DIR} https://raw.githubusercontent.com/${REPO}/master/frps/frps.service
+cp frps ${FRP_DIR} && \
+cp frps.ini ${FRP_CONF_DIR} && \
+cp ./system/frps.service ${SERVICE_DIR} && \
 systemctl daemon-reload
 
 # start frps
